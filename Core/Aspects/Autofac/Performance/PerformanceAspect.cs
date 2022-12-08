e47d03b086a02057a4e6bfe9f1ef77c047d29ca0
berkay çetinkaya <berkayfastsoft@gmail.com>
2022-12-08 17:59:40 +0300

    18:00 last

--- Core/Aspects/Autofac/Performance/PerformanceAspect.cs
+++ Core/Aspects/Autofac/Performance/PerformanceAspect.cs
@@ -7,6 +7,7 @@ using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace Core.Aspects.Autofac.Performance
 {
